1c2b7f72e22af5ae77452c1097c349aff4f0708c
Егор <patron6813@gmail.com>
2019-03-17 12:40:16 +0300

    prod

---
+++ build/precache-manifest.52ae642c4d78d2282c82167c21ce7246.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cebf31a042ee8387172d247e63f1df14",
+    "url": "/static/media/night.cebf31a0.jpg"
+  },
+  {
+    "revision": "78ea50eba5a7ade2444577bcc7074cd7",
+    "url": "/static/media/day.78ea50eb.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "90eb345defdc5bba0731",
+    "url": "/static/js/main.90eb345d.chunk.js"
+  },
+  {
+    "revision": "8a02ba053dd8d7769f27",
+    "url": "/static/js/2.8a02ba05.chunk.js"
+  },
+  {
+    "revision": "90eb345defdc5bba0731",
+    "url": "/static/css/main.20c2c9e2.chunk.css"
+  },
+  {
+    "revision": "5dce5a250e6917f60e8edbefec5d4c0b",
+    "url": "/index.html"
+  }
+];
